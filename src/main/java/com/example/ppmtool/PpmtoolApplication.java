3060741c71ae3020a143e955e5b2972737bce585
Yuval <uvgur931@gmail.com>
2019-12-26 14:39:22 +0200

    v1

---
+++ src/main/java/com/example/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
