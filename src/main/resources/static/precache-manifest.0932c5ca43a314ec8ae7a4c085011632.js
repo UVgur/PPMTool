4f9f45074dabf7c766ad5a5969848b7f1e501662
Yuval <uvgur931@gmail.com>
2020-01-17 21:29:57 +0200

    added react build

---
+++ src/main/resources/static/precache-manifest.0932c5ca43a314ec8ae7a4c085011632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c91808fefa483544c4fa371677ee4f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0572cf9cabb2d409631",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9c52f9ab173330ce90b1",
+    "url": "/static/css/main.a7dc2f72.chunk.css"
+  },
+  {
+    "revision": "f0572cf9cabb2d409631",
+    "url": "/static/js/2.312cfa32.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.312cfa32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c52f9ab173330ce90b1",
+    "url": "/static/js/main.77775254.chunk.js"
+  },
+  {
+    "revision": "0e0de284a6b544b8ab0b",
+    "url": "/static/js/runtime-main.df9c3e05.js"
+  }
+]);
